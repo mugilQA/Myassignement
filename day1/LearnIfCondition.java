3ec103daafd7de93ddba9eab913c14dc45003465
mugilQA <168301331+mugilQA@users.noreply.github.com>
2024-04-27 14:17:36 -0500

    day1

---
+++ day1/LearnIfCondition.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class LearnIfCondition {
+
+	public static void main(String[] args) {
+	int input=-10;
+	if (input>0) {
+		System.out.println("The number is positive");
+	}else if(input<0) {
+		System.out.println("The number is Negative");
+	}else {
+	System.out.println("The number is zero");
+	}
+	}
+
+}
---
+++ day1/LearnSwitch.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class LearnSwitch {
+
+	public static void main(String[] args) {
+		char input='i';
+		switch(input) {
+		case 'h': System.out.println("Hello");break;
+		case 't':System.out.println("TestLeaf");break;
+		default: System.out.println("Hello TestLeaf");
+		}
+
+	}
+
+}
